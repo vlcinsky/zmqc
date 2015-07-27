c8ab730f5fd83c2b4f1e9d7ba293eabc37d49a80
Jan Vlčinský <jan.vlcinsky@tamtamresearch.com>
2015-07-27 23:46:06 +0200

    setup.py rewritten to use `pbr` (and `setup.cfg`)
            modified:   .gitignore
            deleted:    MANIFEST.in
            deleted:    VERSION
            new file:   requirements.txt
            new file:   setup.cfg
            modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from setuptools import setup
-import os.path as p
 
-VERSION = open(p.join(p.dirname(p.abspath(__file__)), 'VERSION')).read().strip()
-
-setup(
-    name='zmqc',
-    version=VERSION,
-    description=u'A small but powerful command-line interface to ØMQ.',
-    author='Zachary Voase',
-    author_email='z@zacharyvoase.com',
-    url='http://github.com/zacharyvoase/zmqc',
-    package_dir={'': 'lib'},
-    py_modules=['zmqc'],
-    install_requires=[
-        'pyzmq',
-        'argparse>=1.2.1',
-    ],
-    entry_points={
-        'console_scripts': [
-            'zmqc = zmqc:main',
-        ],
-    },
-)
+setup(setup_requires=["pbr"], pbr=True)
